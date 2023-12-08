e5cf12a5c7988cff73568af75eaac33c9e8195d0
Rajusubba <subba.raju67@yahoo.com>
2023-12-07 21:36:21 -0500

    Add files via upload

---
+++ projects.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title("Optimizing Bike Inventory in London: A Machine Learning Approach")
+
+st.subheader("Introduction")
+st.markdown("""London being one of the major travel destination in Europe, cycling is great way of traveling in London.
+London Bike Share is major bike rental company that provide the bike rental service. As the bike rental service is growing, 
+it is becoming very difficult for company to find how many bike should be in stock. SO, company is wants to solve this 
+problems using the machine learning approach.""")
+
+st.subheader("Data Description")
+
+
+st.markdown("This portion describe the project.")
+
+st.subheader("Methodology")
+
+st.subheader("TResults and Discussion")
+
+st.subheader("Conclusion")
+
